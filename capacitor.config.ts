ac5b3bbb0780d469f8a860ca0c9cb2837bf6f4e6
Jhonatan Berrios <jhonatan.berrios@luparecruiter.com>
2024-10-09 17:07:45 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'virtual-shop',
+  webDir: 'dist'
+};
+
+export default config;
